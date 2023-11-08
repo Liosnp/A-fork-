a7ecbf1c24ff68249f4a8373215035f37c618191
Liosnp <148738346+Liosnp@users.noreply.github.com>
2023-11-08 15:55:42 +0800

    Update main.py
    render

--- main.py
+++ main.py
@@ -213,13 +213,12 @@ def plot_pie_chart():
         .add("", data_pair)
         .set_global_opts(title_opts=opts.TitleOpts(title=f"{selected_area} Area Loan Approval Rates"))
         .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c} ({d}%)"))
+        .render("liuyan_pie.html")
     )
     
     # 使用st_pyecharts在Streamlit中渲染饼图
-    st.pyplot(st_pyecharts(pie_chart))
-    
-
-
+    st_pyecharts(pie_chart)
+    return None
 
     
 #Author:Yuxi Guo
